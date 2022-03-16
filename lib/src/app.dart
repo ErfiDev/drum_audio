7c250881e8576f072c04a260bc896bd973e86d89
Erfan <erfanhanifezade@gmail.com>
2022-03-16 14:04:07 +0330

    install audioplayers pkg

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import './audioPlayer.dart';
+import 'package:just_audio/just_audio.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -11,87 +10,97 @@ class App extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: DecoratedBox(
-              position: DecorationPosition.background,
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                image: DecorationImage(
-                  fit: BoxFit.fitWidth,
-                  image: AssetImage('images/drum.png'),
-                ),
+            position: DecorationPosition.background,
+            decoration: BoxDecoration(
+              color: Colors.yellow,
+              image: DecorationImage(
+                fit: BoxFit.fitWidth,
+                image: AssetImage('images/drum.png'),
               ),
-              child: getAppColumn()),
+            ),
+            child: Buttons(),
+          ),
         ),
       ),
     );
   }
 }
 
-Widget getAppColumn() {
-  return Column(
-    children: [
-      Expanded(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: IconButton(
-                color: Colors.redAccent,
-                onPressed: null,
-                icon: Icon(Icons.home),
+late AudioPlayer player;
+
+class Buttons extends StatelessWidget {
+  @override
+  Widget build(ctx) {
+    player = AudioPlayer();
+    return Column(
+      children: [
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: IconButton(
+                  color: Colors.redAccent,
+                  onPressed: () async {
+                    await player.setAsset('assets/c1.wav');
+                    player.play();
+                  },
+                  icon: Icon(Icons.home),
+                ),
               ),
-            ),
-            Expanded(
-              child: IconButton(
-                color: Colors.redAccent,
-                onPressed: null,
-                icon: Icon(Icons.audiotrack),
+              Expanded(
+                child: IconButton(
+                  color: Colors.redAccent,
+                  onPressed: null,
+                  icon: Icon(Icons.audiotrack),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      Expanded(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: IconButton(
-                color: Colors.white,
-                onPressed: null,
-                icon: Icon(Icons.audiotrack),
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: IconButton(
+                  color: Colors.white,
+                  onPressed: null,
+                  icon: Icon(Icons.audiotrack),
+                ),
               ),
-            ),
-            Expanded(
-              child: IconButton(
-                color: Colors.white,
-                onPressed: null,
-                icon: Icon(Icons.audiotrack),
+              Expanded(
+                child: IconButton(
+                  color: Colors.white,
+                  onPressed: null,
+                  icon: Icon(Icons.audiotrack),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      Expanded(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: IconButton(
-                color: Colors.redAccent,
-                onPressed: null,
-                icon: Icon(Icons.audiotrack),
+        Expanded(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: IconButton(
+                  color: Colors.redAccent,
+                  onPressed: null,
+                  icon: Icon(Icons.audiotrack),
+                ),
               ),
-            ),
-            Expanded(
-              child: IconButton(
-                color: Colors.redAccent,
-                onPressed: null,
-                icon: Icon(Icons.audiotrack),
+              Expanded(
+                child: IconButton(
+                  color: Colors.redAccent,
+                  onPressed: null,
+                  icon: Icon(Icons.audiotrack),
+                ),
               ),
-            ),
-          ],
-        ),
-      )
-    ],
-  );
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
